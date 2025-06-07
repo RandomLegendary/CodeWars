77d80d9e45f272bc32c1cadd60cb4047cb220c78
RandomLegendary <151962492+RandomLegendary@users.noreply.github.com>
2025-06-07 20:08:27 +0200

    Update solution - CodeHub

--- 8-kyu/beginner-series-number-1-school-paperwork/beginner-series-number-1-school-paperwork.py
+++ 8-kyu/beginner-series-number-1-school-paperwork/beginner-series-number-1-school-paperwork.py
@@ -1,5 +1,20 @@
-def paperwork(n, m):
-    if n <= 0 or m <= 0:
-        return 0
+def tribonacci(signature, n):
+    if n == 0:
+        return []
     else:
-        return n * m
+        
+        result = [signature[0], signature[1], signature[2]]
+        
+        temp = 0
+        
+        while n != temp + 3:     
+            if n == temp + 4:
+                result_temp = result[0+temp] + result[1+temp]
+                result.append(result_temp)
+                temp += 1
+            else:
+                result_temp = result[0+temp] + result[1+temp] + result[2+temp] 
+                result.append(result_temp)
+                temp += 1
+        
+        return result
